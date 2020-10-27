5402553c374625ebd4bb33a7391e5d047b4cf232
jsharrell1 <jsharrell1@crimson.ua.edu>
2020-10-26 23:59:07 -0500

    Updated with menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,7 @@
 package com.company;
 
 import java.util.Scanner;
-import java.util.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
@@ -16,64 +14,76 @@ public class Main {
         // back to a basic array after the combination of that and the menu issues. I'd like
         // to come back to this and figure out how to execute the original plan I had for this.
 
-        /*System.out.println("Choose from the following: ");
-        System.out.println("1. Input Substring");
-        System.out.println("2. Exit");*/
+        menu();
+
+    }
 
 
+    public static void menu() {
 
         Scanner keyboard = new Scanner(System.in);
-        //int choice = keyboard.nextInt();
-        //int[] numbers = {keyboard.nextLine()};
-
-        //List<String> list = new ArrayList<String>();
-
-        //list.add(keyboard.next());
-
-        //array with the example set of numbers given in the word doc
-        int[] numbers = {0,0,1,2,0,1,0};
-
-        System.out.println("Substrings that equal 10 from array " + numbers + " within code");
-
-
-        //for loop that runs through the number array set above
-        for (int i = 0; i < numbers.length; i++) {
-            //calls on the test(method that pulls the numbers from the array, the set sum we want to get to
-            test(numbers, i, numbers[i], 10, String.valueOf(numbers[i]));
-        }
-
-    }
-
-
-    //menu();
-
-    // Switch case based menu to choose the option of inputting an array or exiting the application
-
-        /*switch (choice) {
-
-            case 1:
-                test();
-                break;
-
-
-            case 2:
-                System.exit(0);
-                break;
-        }
-
-
-    }
-
-// method to call on the sout statements for the menu
-
-         */
-   /* public static void menu() {
 
         System.out.println("Choose from the following: ");
+        System.out.println();
         System.out.println("1. Input Substring");
+        System.out.println();
         System.out.println("2. Exit");
+        System.out.println();
 
-    }*/
+
+        int choice;
+
+        choice = keyboard.nextInt();
+
+        //while loop for menu that is set to not break until the user enters 2 to exit
+        while (choice != 3) {
+
+            switch (choice) {
+                case 1:
+
+                    System.out.println("Enter the length of the array: ");
+                    System.out.println();
+
+                    //sets up variable for taking in keyboard input from integers entered
+                    int length = keyboard.nextInt();
+
+                    //creates new array for numbers entered to be stored in
+                    int[] newArray = new int[length];
+
+                    System.out.println("Enter the elements of the array: ");
+                    System.out.println();
+
+                    //for loop that stores the numbers entered into the set length of the array, quits input
+                    //once set length is reached
+                    for (int i = 0; i < length; i++) {
+                        newArray[i] = keyboard.nextInt();
+                    }
+
+                    //print statement that lists the users numbers entered
+                    System.out.println(Arrays.toString(newArray));
+
+                    //for loop for the substring of 10 algorithm
+                    for (int i = 0; i < newArray.length; i++) {
+                        //calls on the test(method that pulls the numbers from the array, the set sum we want to get to
+                        test(newArray, i, newArray[i], 10, String.valueOf(newArray[i]));
+                    }
+
+                    //goes back to beginning of menu
+                    menu();
+
+                    break;
+
+                //exit option
+                case 2:
+                    System.out.println("Goodbye");
+                    System.exit(0);
+                    break;
+
+            }
+        }
+
+    }
+
 
     // list of parameters within the method include the ints from the array
     // the int i that is used to run through the for loop, int sum which is the
@@ -82,34 +92,14 @@ public class Main {
     public static void test(int[] arr, int i, int sum, int ten, String substring) {
         for (int j = i + 1; j < arr.length; j++) {
             // if statement that print the combinations from the array that equal 10
+            //if the sum + ineger from "j" for loop equals ten it is marked as true
             if (sum + arr[j] == ten) {
                 System.out.println(substring + " " + String.valueOf(arr[j]));
             }
             // else statement for if the numbers from the array do not equal 10
             else {
-                test(arr, j, sum+arr[j], ten, substring + " " + String.valueOf(arr[j]));
+                test(arr, j, sum + arr[j], ten, substring + " " + String.valueOf(arr[j]));
             }
         }
-
-
-
-
-
-
-
-       /* int sum = 10;
-
-// original logic i tried to use to figure out the substrings equalling to 10
-        for (int i = 0; i < input.length(); i++){
-            for (int j = i + 1; j <= input.length(); j++){
-                sum += input.length();
-                if(sum == 10){
-                    System.out.println("Substring " + input + " is equal to 10");
-                }
-                else{
-                    System.out.println("Substring " + input + " is not equal to 10");
-                }
-            }
-        }*/
     }
 }
